f734770484793eed58e698672d50b0692e9172b3
ankurverma99 <vermaankur032@gmail.com>
2020-10-21 14:04:40 +0530

    alia

--- src/org/qsp/demoinner/Demo.java
+++ src/org/qsp/demoinner/Demo.java
@@ -2,7 +2,7 @@ package org.qsp.demoinner;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Hello");
 	}
 
 }
--- src/org/qsp/demoinner/DemoGit.java
+++ src/org/qsp/demoinner/DemoGit.java
@@ -1,5 +1,7 @@
 package org.qsp.demoinner;
 
 public class DemoGit {
+	public static void main(String[] args) {
 
+	}
 }
